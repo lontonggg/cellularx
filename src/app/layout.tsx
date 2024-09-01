0c35cc17468f2912e236aa7197ce63c304aa2c59
lontonggg <zadareyhan@gmail.com>
2024-09-01 22:50:38 +0700

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "@/styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CellularX",
-  description: "",
+  title: "Cellular X",
+  description: "Cellular X has all the cell phone accessories you need! They have everything from cases to covers in all styles and colours! They also specialize in smart phone repairs! From cracks in your screen to helping to unlock new devices, Cellular X is your place for everything phone related!",
 };
 
 export default function RootLayout({
